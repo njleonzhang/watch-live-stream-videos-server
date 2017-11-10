f11920ad717ce59188e634e2a3f2c5afebf10b7d
njleonzhang <scream3616@sina.com>
2017-11-10 14:28:49 +0800

    remove useless router

--- app/controller/videoRoom.js
+++ app/controller/videoRoom.js
@@ -2,12 +2,12 @@ const jsonRes = require('../util/responseBuilder').jsonRes
 
 module.exports = app => {
   class VideoRoomCtl extends app.Controller {
-    async add() {
-      const { app, ctx } = this
-      console.log(ctx.request.body)
-      const result = await ctx.service.videoRoom.add(ctx.request.body)
-      ctx.body = jsonRes(result)
-    }
+    // async add() {
+    //   const { app, ctx } = this
+    //   console.log(ctx.request.body)
+    //   const result = await ctx.service.videoRoom.add(ctx.request.body)
+    //   ctx.body = jsonRes(result)
+    // }
   }
 
   return VideoRoomCtl
--- app/router.js
+++ app/router.js
@@ -4,7 +4,7 @@ module.exports = app => {
   app.post('/auth/login', 'auth.login')
   app.post('/auth/register', 'auth.register')
   app.get('/auth/logout', 'auth.logout')
-  app.post('/video_room/add', 'videoRoom.add')
+  // app.post('/video_room/add', 'videoRoom.add')
   app.post('/user/add_video_room', 'user.addVideoRoom')
   app.get('/user/get_rooms', 'user.getRooms')
 }
